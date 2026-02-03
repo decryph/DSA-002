627e9516b8b79344e6da51a82b76a86e80ab1103
Shruti Naik <158175549+decryph@users.noreply.github.com>
2026-02-03 21:02:34 +0530

    Time: 3 ms (79.97%), Space: 11.6 MB (26.89%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+         if (s.empty() || t.empty()) {
+            return "";
+        }
+
+        vector<int> freq(256);
+        for(int i=0;i<t.size();i++){
+            freq[t[i]]++;
+        }
+
+        int minSize = INT_MAX, count = 0, sIndex = 0, l = 0, r = 0;
+
+        while(r < s.size()){
+            if(freq[s[r]] > 0)
+                count++;
+            freq[s[r]]--;
+            while(count==t.size()){
+                if(r-l+1 < minSize){
+                    minSize = r-l+1;
+                    sIndex = l;
+                }
+                freq[s[l]]++;
+                if(freq[s[l]] > 0)
+                    count--;
+                l++;
+            }
+            r++;
+        }
+
+        if(sIndex == 0 && minSize == INT_MAX)
+            return "";
+        return s.substr(sIndex, minSize);
+    }
+};
