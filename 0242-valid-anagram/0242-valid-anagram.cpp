4fe3847f7547f4f5c72b1f267731ff3b356675e3
Shruti Naik <158175549+decryph@users.noreply.github.com>
2026-02-05 00:10:51 +0530

    Time: 2 ms (51.29%), Space: 9.8 MB (25.03%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length() != t.length()) return false;
+
+        unordered_map<char,int> mpp;
+
+        for(char c : s) mpp[c]++;
+        for(char c : t) mpp[c]--;
+
+        for(auto &it : mpp) {
+            if(it.second != 0) return false;
+        }
+
+        return true;
+    }
+};
