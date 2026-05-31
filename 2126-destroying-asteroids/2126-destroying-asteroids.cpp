9cb5ad8d55feca34a261935f05e2d7c172dde4b6
Shruti Naik <shrutirn02@gmail.com>
2026-05-31 22:44:36 +0530

    Time: 27 ms (84.23%), Space: 106.7 MB (81.61%) - LeetHub

---
+++ 2126-destroying-asteroids/2126-destroying-asteroids.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool asteroidsDestroyed(int mass, vector<int>& asteroids) {
+        sort(begin(asteroids), end(asteroids));
+
+        for(int &m : asteroids){
+            if(mass < m) return false;
+            else if (mass >= 1e5) return true;
+
+            mass += m;
+        }
+        return true;
+    }
+};
