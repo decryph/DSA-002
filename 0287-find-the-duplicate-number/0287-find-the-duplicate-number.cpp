5de5372b47625517f8be05065e1f2578d67ff4da
Shruti Naik <shrutirn02@gmail.com>
2026-06-04 22:29:51 +0530

    Time: 4 ms (59.97%), Space: 65 MB (90.4%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -9,7 +9,7 @@ public:
         fast = nums[nums[fast]];
      }while(slow != fast);
 
-     slow = nums[0];
+     fast = nums[0];
      while(slow != fast){
         slow = nums[slow];
         fast = nums[fast];
