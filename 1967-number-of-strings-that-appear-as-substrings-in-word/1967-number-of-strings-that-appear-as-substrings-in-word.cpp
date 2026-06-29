359dc4628ff66205126e56f03edd257d49d69ea1
Shruti Naik <shrutirn02@gmail.com>
2026-06-29 18:16:52 +0530

    Time: 0 ms (100%), Space: 14.4 MB (9.47%) - LeetHub

---
+++ 1967-number-of-strings-that-appear-as-substrings-in-word/1967-number-of-strings-that-appear-as-substrings-in-word.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+
+void LPS(string ptrn, vector<int>& lps){
+    int M = ptrn.length();
+    int len =0;
+    lps[0] =0;
+
+    int i =1;
+    while(i < M){
+        if(ptrn[i] == ptrn[len]){
+            len++;
+            lps[i] = len;
+            i++;
+        } else{
+            if(len != 0){
+                len = lps[len-1];
+            } else{
+                lps[i] =0;
+                i++;
+            }
+        }
+    }
+}
+
+vector<int> srch(string pat,string txt){
+    int N = txt.length();
+    int M = pat.length();
+    vector<int> result;
+
+    vector<int> lps(M,0);
+    LPS(pat,lps);
+
+    int i =0,j =0;
+    while(i<N){
+        if(pat[j] == txt[i]){
+            i++;
+            j++;
+        }
+        if(j == M){
+            result.push_back(i-j+1);
+            j = lps[j-1];
+        } else if(i < N && pat[j] != txt[i]){
+            if( j != 0){
+                j = lps[j-1];
+            } else{
+                i++;
+            }
+        }
+    } 
+    return result;
+}
+    int numOfStrings(vector<string>& patterns, string word) {
+        int cnt =0;
+        for(const string& pat: patterns){
+            if(!srch(pat, word).empty()){
+            cnt++;}
+        }
+        return cnt;
+    }
+};
