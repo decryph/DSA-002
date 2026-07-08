2d0f421141ea1982a5bc0d1c4d31b3d08c6079ed
Shruti Naik <shrutirn02@gmail.com>
2026-07-08 18:47:06 +0530

    Time: 31 ms (83.94%), Space: 155.4 MB (52.33%) - LeetHub

---
+++ 3756-concatenate-non-zero-digits-and-multiply-by-sum-ii/3756-concatenate-non-zero-digits-and-multiply-by-sum-ii.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+   int MOD = 1e9 +7;
+   typedef long long ll;
+    vector<int> sumAndMultiply(string s, vector<vector<int>>& queries) {
+        int n = s.length();
+
+        vector<int> nonZerocnt(n,0);
+        vector<ll> numberupto(n,0);
+        vector<ll> digsumupto(n,0);
+        vector<ll> pow10(n+1,0);
+
+        pow10[0] =1;
+        for(int i =1;i<= n; i++){
+            pow10[i] = (pow10[i-1]*10)%MOD;
+        }
+
+        nonZerocnt[0] = (s[0] != '0')? 1:0;
+        for(int i =1;i<n;i++){
+            int dig = s[i]-'0';
+            nonZerocnt[i] = nonZerocnt[i-1]+((dig !=0)? 1:0);
+        }
+        numberupto[0] = s[0]-'0';
+        for(int i =1; i<n;i++){
+            int digi = s[i]-'0';
+            if(digi != 0){
+                numberupto[i] = ((numberupto[i-1]*10)+digi)%MOD;
+            } else{
+                numberupto[i] = numberupto[i-1];
+            }
+        }
+
+        digsumupto[0] = s[0]-'0';
+        for(int i=1;i<n;i++){
+            int digit = s[i] -'0';
+            digsumupto[i] = digsumupto[i-1]+digit;
+        }
+
+        int q = queries.size();
+        vector<int>res(q);
+        for(int i =0; i<q;i++){
+            int l = queries[i][0];
+            int r = queries[i][1];
+            int k = nonZerocnt[r] - ((l==0) ? 0 : nonZerocnt[l-1]);
+            int numb = (l ==0) ? 0: numberupto[l-1];
+
+            ll sum = digsumupto[r] - ((l==0) ?0 : digsumupto[l-1]);
+            ll x = (numberupto[r]- (numb* pow10[k] % MOD)+ MOD)%MOD;
+            res[i] = (int)((x*sum)%MOD);
+
+        }
+return res;
+    }
+};
