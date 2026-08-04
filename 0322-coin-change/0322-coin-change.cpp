ee021808af16226bc9f4ec21100eab25b78e22b7
Shruti Naik <shrutirn02@gmail.com>
2026-08-04 23:38:00 +0530

    Time: 41 ms (45.67%), Space: 52.9 MB (14.92%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+
+        int n = coins.size();
+
+        vector<vector<int>> t(n + 1, vector<int>(amount + 1));
+
+        for(int i = 0; i <= n; i++)
+            t[i][0] = 0;
+
+        for(int j = 1; j <= amount; j++)
+            t[0][j] = INT_MAX - 1;
+
+        // Second row initialization
+        for(int j = 1; j <= amount; j++) {
+            if(j % coins[0] == 0)
+                t[1][j] = j / coins[0];
+            else
+                t[1][j] = INT_MAX - 1;
+        }
+
+        for(int i = 2; i <= n; i++) {
+            for(int j = 1; j <= amount; j++) {
+
+                if(coins[i-1] <= j)
+                    t[i][j] = min(1 + t[i][j - coins[i-1]],
+                                  t[i-1][j]);
+                else
+                    t[i][j] = t[i-1][j];
+            }
+        }
+
+        if(t[n][amount] == INT_MAX - 1)
+            return -1;
+
+        return t[n][amount];
+    }
+};
