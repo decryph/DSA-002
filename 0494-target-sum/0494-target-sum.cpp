191872fb139af69fc664809cf7d6a4c7d8f7c912
Shruti Naik <shrutirn02@gmail.com>
2026-08-04 19:17:02 +0530

    Time: 0 ms (100%), Space: 11.7 MB (80.32%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+int isSubset(vector<int>& arr, int n, int sum) {
+    int t[n+1][sum+1];
+
+    for(int i = 0; i <= n; i++){
+        for(int j = 0; j <= sum; j++){
+            if(i == 0)
+                t[i][j] = 0;
+            if(j == 0)
+                t[i][j] = 1;
+        }
+    }
+
+    for(int i = 1; i <= n; i++){
+        for(int j = 0; j <= sum; j++){
+            if(arr[i-1] <= j)
+                t[i][j] = t[i-1][j-arr[i-1]] + t[i-1][j];
+            else
+                t[i][j] = t[i-1][j];
+        }
+    }
+
+   return t[n][sum];
+}
+    int findTargetSumWays(vector<int>& nums, int target) {
+        target = abs(target);
+        int n = nums.size();
+        int sum =0;
+        for(int i =0; i<n; i++){
+            sum += nums[i];
+        } if(sum < target || (sum + target) %2 != 0){
+            return 0;
+        }
+        return isSubset(nums,n,(sum+target)/2 );
+    }
+};
